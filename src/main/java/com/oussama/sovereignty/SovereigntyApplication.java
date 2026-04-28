ee10ad23f1932f03331c7a69be7295fbe28517ef
Oussama LAHMIDI <oussama.lahmidi@icloud.com>
2026-04-28 23:50:05 +0200

    feature: Initial codebase

---
+++ src/main/java/com/oussama/sovereignty/SovereigntyApplication.java
@@ -0,0 +1,13 @@
+package com.oussama.sovereignty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SovereigntyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SovereigntyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oussama/sovereignty/SovereigntyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oussama.sovereignty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SovereigntyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
